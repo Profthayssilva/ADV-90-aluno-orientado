e0f033d28ed76174b8f3f1962257d8e5aa7a454f
Thays Silva <113531216+Profthayssilva@users.noreply.github.com>
2023-03-19 20:05:04 -0300

    Add files via upload

---
+++ game_login.js
@@ -0,0 +1,9 @@
+// Defina a função onclick addUser().
+
+// Obtenha os valores de entrada das caixas de entrada de player1 e player2 e armazene esses valores
+// dentro das variáveis.
+
+// Então, estamos armazenando o valor das variáveis player1Name e player2Name com a respectiva
+// chave no armazenamento local usando a função setItem()
+
+// agora, redirecionando para game_page.html .
